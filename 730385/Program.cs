9501a437f52d65ee98c2a52ae209681478758cab
visualstudio <visualstudio@122-PC22>
2019-04-17 08:43:42 -0400

    starter code

--- 730385/Program.cs
+++ 730385/Program.cs
@@ -10,6 +10,26 @@ namespace _730385
     {
         static void Main(string[] args)
         {
+            new GenerateList().Run(57);
+        }
+        class GenerateList
+        {
+            const int LEN = 100;
+            ArrayList ListQ;
+            public void Run(int AddUpToNumber)
+            {
+                Random r = new Random();
+
+                ListQ = new ArrayList();
+                for (int i = 0; i < LEN; i++)
+                {
+                    ListQ.Add(r.Next(100));
+                }
+                this.findTwoNumbersThatAddUpToNumber);
+            }
         }
     }
-}
+
+            
+        
+        
